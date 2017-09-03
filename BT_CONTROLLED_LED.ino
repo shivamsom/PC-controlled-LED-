735e7316dded5ca3c6c90b57280e0a7d09c61f35
Shivam Som <shivamsom3@gmail.com>
2017-09-03 13:55:02 +0530

    Add files via upload

---
+++ BT_CONTROLLED_LED.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+Serial.begin(9600);
+pinMode(12,OUTPUT);  
+}
+void loop()
+{
+  char c = Serial.read();  
+
+  switch(c)
+  {
+    case '1': digitalWrite(12,HIGH);
+              Serial.print("ON");
+                break;
+    case '0':digitalWrite(12,LOW);
+              Serial.print("OFF");
+              break;            
+  }
+  
+}
